ddfb95086ea1fbaff83c7344657e1fcf82d56a74
JE3 <JE3@DESKTOP-2MSRQI6>
2023-10-05 14:24:57 +0530

    New Test case is created and Modifying the existing test case.This code is done by Aditya Sonar.

---
+++ src/gitGitHub/TestCase2.java
@@ -0,0 +1,11 @@
+package gitGitHub;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("This is Test Case 2");
+		
+	}
+
+}
